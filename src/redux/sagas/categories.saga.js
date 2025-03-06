a1f5e75c3d0de5cdfcebd3f7681c971f05c14e1f
Shobha Chejara <shobha.chejara@gmail.com>
2025-03-06 13:02:00 -0600

    Created Category saga and integrated into root.saga.js

---
+++ src/redux/sagas/categories.saga.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+import {put, takeLatest} from  "redux-saga/effects";
+function*fetchCategories(){
+try{
+    const categoriesResponse  = yield axios.get('/api/categories');
+    yield put({type: 'SET_CATEGORIES', payload:  categoriesResponse.data})
+    console.log('Fetched Categories:',  categoriesResponse .data);
+
+}
+catch(error){
+    console.log('error in fetching categories');
+}
+
+}
+
+function*categoriesSaga (){
+yield takeLatest('FETCH_CATEGORIES',fetchCategories);
+
+}
+export default categoriesSaga;

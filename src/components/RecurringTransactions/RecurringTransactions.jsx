7239e90ed3d11af07653c5f2b9bfb25c5d4a5668
Shobha Chejara <shobha.chejara@gmail.com>
2025-04-16 13:27:49 -0500

    Created Recurring Transactions component

---
+++ src/components/RecurringTransactions/RecurringTransactions.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function RecurringTransactions (){
+return(
+<div>
+    <h1>This is Recurring Transactions Page</h1>
+</div>
+
+)
+
+}
+export default RecurringTransactions;

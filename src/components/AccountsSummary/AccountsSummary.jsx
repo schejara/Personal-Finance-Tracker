45eb2c95c33eb80c4d4139fe2a6e0ec871746e20
Shobha Chejara <shobha.chejara@gmail.com>
2025-06-09 13:48:44 -0500

    Initial Setup AccountsSummary Component

---
+++ src/components/AccountsSummary/AccountsSummary.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AccountsSummary(){
+    return(
+        <div>
+        <h1>Account Summary page</h1>
+        </div>
+    )
+}
+
+export default AccountsSummary;
